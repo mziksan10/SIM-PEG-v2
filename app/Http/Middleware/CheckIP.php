31b0a9e50c10653c1505b6eaacc2eabd4508f0af
mziksan10 <mziksan10@gmail.com>
2023-08-12 14:48:53 +0700

    revisi file .gitignore

---
+++ app/Http/Middleware/CheckIP.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckIP
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->ip() >= '127.0.0.*') {
+            return $next($request);
+        }
+        
+        return redirect('/')->with('failed', 'Gunakan alamat IP Address yang telah terdaftar di sistem!');
+
+    }
+}
